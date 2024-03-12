cb518261451c08ed9f326d1011c1024dfede48df
patryk.kordel@gmail.com <patryk.kordel@gmail.com>
2024-03-12 01:07:13 +0100

    12.03.2024

--- WPFNotepad/MainWindow.xaml.cs
+++ WPFNotepad/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using Microsoft.Win32;
 using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
 
 namespace WPFNotepad
 {
